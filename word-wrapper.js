2621ead8980f7b759072a3cd139febea93547893
Jonathan Commins <joncom@gmail.com>
2015-11-12 14:17:14 -0800

    Update module path

--- word-wrapper.js
+++ word-wrapper.js
@@ -1,4 +1,4 @@
-ig.module('game.plugins.WordWrapper').requires('impact.font').defines(function() {
+ig.module('game.plugins.word-wrapper').requires('impact.font').defines(function() {
     WordWrapper = ig.Class.extend({
         font: null,
         box: null,
